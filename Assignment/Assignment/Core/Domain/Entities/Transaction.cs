735be3f2ce914919321b8514130b24cd7ece789b
Jakkavat Deemak <jd@manaosoftware.com>
2019-07-10 10:26:00 +0700

    Add Unit test project and add customer/transaction entity

---
+++ Assignment/Assignment/Core/Domain/Entities/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Core.Domain.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public TransactionStatus Status { get; set; }
+    }
+
+    public enum TransactionStatus
+    {
+        Success,
+        Failed,
+        Canceled
+    }
+
+}
